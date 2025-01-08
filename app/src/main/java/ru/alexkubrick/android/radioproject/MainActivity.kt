61c4e4298bc37a1ef767c4b8f421975dac6c6aff
AlexKamenchuk <alexandra.kamenchyk@mail.ru>
2025-01-09 00:53:06 +0700

    главный экран изменили кнопку

--- app/src/main/java/ru/alexkubrick/android/radioproject/MainActivity.kt
+++ app/src/main/java/ru/alexkubrick/android/radioproject/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.playPauseButton.text = "Играть"
-        binding.playPauseButton.setOnClickListener {
+        binding.bPlay.setImageResource(R.drawable.ic_play)
+        binding.bPlay.setOnClickListener {
             manageRadioService()
         }
     }
@@ -26,10 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         if (!isPlaying) {
             serviceIntent.action = RadioConstants.ACTION.STARTFOREGROUND_ACTION
-            binding.playPauseButton.text = "Остановить"
+            binding.bPlay.setImageResource(R.drawable.ic_stop)
         } else {
             serviceIntent.action = RadioConstants.ACTION.STOPFOREGROUND_ACTION
-            binding.playPauseButton.text = "Играть"
+            binding.bPlay.setImageResource(R.drawable.ic_play)
         }
 
         startService(serviceIntent)
