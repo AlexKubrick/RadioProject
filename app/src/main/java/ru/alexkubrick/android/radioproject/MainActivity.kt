8c7e92be5d3a85d9401f3a58e6fcd98f5e0a5ec8
AlexKamenchuk <alexandra.kamenchyk@mail.ru>
2025-01-13 00:11:26 +0700

    seekBar работает

--- app/src/main/java/ru/alexkubrick/android/radioproject/MainActivity.kt
+++ app/src/main/java/ru/alexkubrick/android/radioproject/MainActivity.kt
@@ -2,47 +2,156 @@ package ru.alexkubrick.android.radioproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.SeekBar
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.HttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import ru.alexkubrick.android.radioproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var isPlaying = false
+    private lateinit var player: ExoPlayer
 
+    private fun getTimeString(duration: Int): String {
+        val min = duration / 60
+        val sec = duration % 60
+        val time = String.format("%02d:%02d",min,sec)
+        return time
+    }
 
+    @OptIn(UnstableApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+        player =
+            ExoPlayer.Builder(this)
+                .setMediaSourceFactory(DefaultMediaSourceFactory(this).setLiveTargetOffsetMs(5000))
+                .build()
+
+        val mediaItem =
+            MediaItem.Builder()
+                .setUri(RadioConstants.RADIOURL)
+                .setLiveConfiguration(
+                    MediaItem.LiveConfiguration.Builder().setMaxPlaybackSpeed(1.02f).build()
+                )
+                .build()
+        player.setMediaItem(mediaItem)
+        player.prepare()
+
         binding.twCurrentStation.text = getString(R.string.radio_kniga)
+        binding.twTime.text = "00:00"
         binding.bPlay.setImageResource(R.drawable.ic_play)
         binding.bPlay.setOnClickListener {
             manageRadioService()
         }
     }
 
+    @OptIn(UnstableApi::class)
     private fun manageRadioService() {
-        val serviceIntent = Intent(this, RadioPlayerService::class.java)
-
+        //val serviceIntent = Intent(this, RadioPlayerService::class.java)
         if (!isPlaying) {
-            serviceIntent.action = RadioConstants.ACTION.STARTFOREGROUND_ACTION
-            binding.bPlay.setImageResource(R.drawable.ic_stop)
+            //serviceIntent.action = RadioConstants.ACTION.STARTFOREGROUND_ACTION
+            player.play()
         } else {
-            serviceIntent.action = RadioConstants.ACTION.STOPFOREGROUND_ACTION
-            binding.bPlay.setImageResource(R.drawable.ic_play)
+            //serviceIntent.action = RadioConstants.ACTION.STOPFOREGROUND_ACTION
+            player.pause()
         }
-
-        startService(serviceIntent)
+        //startService(serviceIntent)
         isPlaying = !isPlaying
+
+        player.addListener(object : Player.Listener {
+            @Deprecated("Deprecated in Java")
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                if (playbackState == Player.STATE_READY && player.playWhenReady) {
+                    binding.bPlay.setImageResource(R.drawable.ic_stop)
+                } else {
+                    binding.bPlay.setImageResource(R.drawable.ic_play)
+                }
+            }
+
+            override fun onPositionDiscontinuity(
+                oldPosition: Player.PositionInfo,
+                newPosition: Player.PositionInfo,
+                reason: Int,
+            ) {
+                val currentPosition = player.currentPosition.toInt() / 1000
+                binding.playerSeekBar.progress = currentPosition
+                binding.twTime.text = getTimeString(currentPosition)
+            }
+
+        })
+
+        binding.playerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    player.seekTo(progress.toLong() *1000)
+                    binding.twTime.text= getTimeString(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+//        player.addListener(
+//            object : Player.Listener {
+//                override fun onPlayerError(error: PlaybackException) {
+//                    val cause = error.cause
+//                    if (cause is HttpDataSource.HttpDataSourceException) {
+//                        val httpError = cause
+//                        if (httpError is HttpDataSource.InvalidResponseCodeException) {
+//                        } else {
+//                        }
+//                    }
+//                }
+//            }
+//        )
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.post(object : Runnable{
+            override fun run() {
+                val currentposition = player.currentPosition.toInt() / 1000
+                binding.playerSeekBar.progress=currentposition
+                binding.twTime.text = getTimeString(currentposition)
+                handler.postDelayed(this,1000)
+            }
+
+        })
     }
+//        player.addListener(
+//            object : Player.Listener {
+//                override fun onIsPlayingChanged(isPlaying: Boolean) {
+//                    if (isPlaying) {
+//                        binding.bPlay.setImageResource(R.drawable.ic_stop)
+//                    } else {
+//                        binding.bPlay.setImageResource(R.drawable.ic_play)
+//                        // Not playing because playback is paused, ended, suppressed, or the player
+//                        // is buffering, stopped or failed. Check player.playWhenReady,
+//                        // player.playbackState, player.playbackSuppressionReason and
+//                        // player.playerError for details.
+//                    }
+//                }
+//            }
+//        )
 
-    private fun controlRadio() {
-
-    }
-
-
-    companion object {
-        var RadioPlayerServiceInstance: RadioPlayerService? = null
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
     }
 }
--- app/src/main/java/ru/alexkubrick/android/radioproject/RadioPlayerService.kt
+++ app/src/main/java/ru/alexkubrick/android/radioproject/RadioPlayerService.kt
@@ -1,78 +1,71 @@
 package ru.alexkubrick.android.radioproject
-
-import android.app.*
-import android.content.Intent
-import android.os.IBinder
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.media3.common.MediaItem
-import androidx.media3.common.MediaMetadata
-import androidx.media3.common.Player
-import androidx.media3.exoplayer.ExoPlayer
-
-class RadioPlayerService : Service() {
-    private lateinit var player: ExoPlayer
-    private var isPlaying = false
-
-    private val mediaMetadata = MediaMetadata.Builder()
-        .setDisplayTitle("Неизвестная станция")
-        .build()
-
-    override fun onCreate() {
-        super.onCreate()
-        player = ExoPlayer.Builder(this).build()
-        val mediaItem = MediaItem.fromUri(RadioConstants.RADIOURL)
-        player.setMediaItem(mediaItem)
-        player.prepare()
-    }
-
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        if (intent.action == RadioConstants.ACTION.STARTFOREGROUND_ACTION) {
-            startForeground(RadioConstants.NOTIFICATION_ID, createNotification())
-            playMusic()
-            isPlaying = true
-        } else if (intent.action == RadioConstants.ACTION.STOPFOREGROUND_ACTION) {
-            stopMusic()
-            stopForeground(true)
-            isPlaying = false
-            stopSelf()
-        }
-        return START_NOT_STICKY
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        player.release()
-    }
-
-    override fun onBind(intent: Intent): IBinder? {
-        return null
-    }
-
-    private fun createNotification(): Notification {
-        val stopIntent = Intent(this, RadioPlayerService::class.java).apply {
-            action = RadioConstants.ACTION.STOPFOREGROUND_ACTION
-        }
-        val pendingStopIntent = PendingIntent.getService(this, 0, stopIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        return NotificationCompat.Builder(this, RadioConstants.NOTIFICATION_CHANNEL_ID)
-            .setContentTitle(getString(R.string.radio_kniga))
-            .setContentText("Играет")
-            .setSmallIcon(R.drawable.ic_radio_player)
-            .addAction(R.drawable.ic_radio_player, "Остановить", pendingStopIntent)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-            .build()
-    }
-
-    private fun playMusic() {
-        if (!player.isPlaying) {
-            player.play()
-        }
-    }
-
-    private fun stopMusic() {
-        if (player.isPlaying) {
-            player.stop()
-        }
-    }
-}
+//
+//import android.app.*
+//import android.content.Intent
+//import android.os.IBinder
+//import androidx.core.app.NotificationCompat
+//import androidx.media3.common.MediaItem
+//import androidx.media3.exoplayer.ExoPlayer
+//
+//class RadioPlayerService : Service() {
+//    private lateinit var player: ExoPlayer
+//    private var isPlaying = false
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        player = ExoPlayer.Builder(this).build()
+//        val mediaItem = MediaItem.fromUri(RadioConstants.RADIOURL)
+//        player.setMediaItem(mediaItem)
+//        player.prepare()
+//    }
+//
+//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+//        if (intent.action == RadioConstants.ACTION.STARTFOREGROUND_ACTION) {
+//            startForeground(RadioConstants.NOTIFICATION_ID, createNotification())
+//            playMusic()
+//            isPlaying = true
+//        } else if (intent.action == RadioConstants.ACTION.STOPFOREGROUND_ACTION) {
+//            stopMusic()
+//            stopForeground(true)
+//            isPlaying = false
+//            stopSelf()
+//        }
+//        return START_NOT_STICKY
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        player.release()
+//    }
+//
+//    override fun onBind(intent: Intent): IBinder? {
+//        return null
+//    }
+//
+//    private fun createNotification(): Notification {
+//        val stopIntent = Intent(this, RadioPlayerService::class.java).apply {
+//            action = RadioConstants.ACTION.STOPFOREGROUND_ACTION
+//        }
+//        val pendingStopIntent = PendingIntent.getService(this, 0, stopIntent, PendingIntent.FLAG_IMMUTABLE)
+//
+//        return NotificationCompat.Builder(this, RadioConstants.NOTIFICATION_CHANNEL_ID)
+//            .setContentTitle(getString(R.string.radio_kniga))
+//            .setContentText("Играет")
+//            .setSmallIcon(R.drawable.ic_radio_player)
+//            .addAction(R.drawable.ic_radio_player, "Остановить", pendingStopIntent)
+//            .setPriority(NotificationCompat.PRIORITY_LOW)
+//            .build()
+//    }
+//
+//    private fun playMusic() {
+//        if (!player.isPlaying) {
+//            player.play()
+//        }
+//    }
+//
+//    private fun stopMusic() {
+//        if (player.isPlaying) {
+//            player.stop()
+//        }
+//    }
+//}
